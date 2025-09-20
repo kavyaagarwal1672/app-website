ae7d0335e443b5c77064d61d46cd239c55d58a0f
kavyaagarwal1672 <agkavya988@gmail.com>
2025-09-20 09:42:45 +0530

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,23 @@
+const lectures = [
+  {
+    thumb: "thumbnails/intro-to-cn.jpg",
+    title: "Introduction To Computer Networks, Lecture 001",
+    channel: "channel-pictures/channel-1.jpeg",
+  },
+   {
+    thumb: "thumbnails/intro-to-maths.jpg",
+    title: "Introduction To Math , Lecture 001",
+    channel: "channel-pictures/channel-2.jpeg",
+  },
+   {
+    thumb: "thumbnails/intro-to-ds.jpg",
+    title: "Introduction To Data science , Lecture 001",
+    channel: "channel-pictures/channel-6.jpeg",
+  },
+   {
+    thumb: "thumbnails/intro-to-os.png",
+    title: "Introduction To Operating System , Lecture 001",
+    channel: "channel-pictures/channel-4.jpeg",
+  },
+  
+];
