320b24038d93e4e6978b5a0d3d2b5024498eeb26
kavyaagarwal1672 <agkavya988@gmail.com>
2025-09-28 08:04:10 +0530

    Update data.js

--- data/data.js
+++ data/data.js
@@ -21,3 +21,95 @@ const lectures = [
   },
   
 ];
+
+
+const attendanceRecords = [
+  {
+    code: "BAS007B",
+    name: "Discrete Mathematics",
+    type: "Theory",
+    total: 15,
+    present: 11
+  },
+  {
+    code: "BC0008B",
+    name: "Operating Systems",
+    type: "Theory",
+    total: 15,
+    present: 10
+  },
+  {
+    code: "BC0011A",
+    name: "Computer Networks",
+    type: "Theory",
+    total: 20,
+    present: 17
+  },
+  {
+    code: "BC0014B",
+    name: "Operating Systems Lab",
+    type: "Practical",
+    total: 5,
+    present: 3
+  },
+  {
+    code: "BC0081B",
+    name: "Python Programming",
+    type: "Theory",
+    total: 15,
+    present: 8
+  },
+  {
+    code: "BC0082B",
+    name: "Python Programming Lab",
+    type: "Practical",
+    total: 5,
+    present: 3
+  },
+  {
+    code: "BC0594A",
+    name: "Computer Network Lab",
+    type: "Practical",
+    total: 5,
+    present: 4
+  },
+  {
+    code: "BC0606A",
+    name: "Introduction to Data Science",
+    type: "Theory",
+    total: 15,
+    present: 11
+  }
+];
+
+
+const users = [
+  { email: "kavya@gmail.com", 
+    password: "kavya123" },
+
+  { email: "tarun@gmail.com",
+    password: "tarun123" },
+
+  { email: "swasti@gmail.com", 
+    password: "swasti123" },
+
+  { email: "chirag@gmail.com", 
+    password: "chirag123" },
+
+  { email: "siddarth@gmail.com", 
+    password: "siddarth123" },
+
+  { email: "priyanshi@gmail.com", 
+    password: "priyanshi123" }
+];
+
+
+const validSignUps = [
+  { email: "kavya@gmail.com", password: "Kavya$Secure123!" },
+  { email: "tarun@gmail.com", password: "tarun123" },
+  { email: "swasti@gmail.com", password: "swasti123" },
+  { email: "chirag@gmail.com", password: "chirag123" },
+  { email: "siddarth@gmail.com", password: "siddarth123" },
+  { email: "priyanshi@gmail.com", password: "priyanshi123" }
+];
+
